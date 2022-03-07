07cf6c62875b03404350ff79963cc03709b670ee
Todor Vretenarov <vretenarovtodor@gmail.com>
2022-03-06 22:40:57 -0500

    bootstrap cdn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,23 @@ import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-// import Script from 'next/script';
-import { useEffect } from 'react';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    typeof document !== undefined ? require('bootstrap/dist/js/bootstrap') : null;
-  }, []);
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+        />
       </Head>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      />
       <Component {...pageProps} />
     </>
   );
