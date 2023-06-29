1e93015fb0ac215b3fabf78882a6542fb8482923
Todor Vretenarov <vretenarovtodor@gmail.com>
2023-06-28 23:27:44 -0400

    feat: vite refactor

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
